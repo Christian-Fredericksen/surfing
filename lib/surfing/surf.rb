698ab1c38db3bc7871bd29fe0b5ded112a9c9b88
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-02-08 19:40:08 +0000

    added elements to objects

--- lib/surfing/surf.rb
+++ lib/surfing/surf.rb
@@ -7,62 +7,82 @@ class Surf
   surf_spot1 = self.new
   surf_spot1.name = "Windansea Beach, La Jolla"
   surf_spot1.url = "https://en.wikipedia.org/wiki/Arapahoe_Basin"
-  surf_spot1.surf_shop = "TBD"
-  surf_spot1.food = "TBD"
+  surf_spot1.shop = "Best Surf Shop"
+  surf_spot1.shop_url = "https://www.surfshop.com"
+  surf_spot1.food = "Eat Here!"
+  surf_spot1.food_url = "https://www.lunchbucket.com"
   
   surf_spot2 = self.new 
   surf_spot2.name = "Swami’s, Encinitasands"
   surf_spot2.url = "https://en.wikipedia.org/wiki/Aspen_Highlands"
-  surf_spot2.surf_shop = "TBD"
-  surf_spot2.food = "TBD"
+  surf_spot2.shop = "Best Surf Shop"
+  surf_spot2.shop_url = "https://www.surfshop.com"
+  surf_spot2.food = "Eat Here!"
+  surf_spot2.food_url = "https://www.lunchbucket.com"
   
   surf_spot3 = self.new 
   surf_spot3.name = "Trestles (San Onofre State Beach), San Clemente"
   surf_spot3.url = "https://en.wikipedia.org/wiki/Aspen_Mountain_(ski_area)"
-  surf_spot3.surf_shop = "TBD"
-  surf_spot3.food = "TBD"
+  surf_spot3.shop = "Best Surf Shop"
+  surf_spot3.shop_url = "https://www.surfshop.com"
+  surf_spot3.food = "Eat Here!"
+  surf_spot3.food_url = "https://www.lunchbucket.com"
   
   surf_spot4 = self.new 
   surf_spot4.name = "The Wedge, Newport Beach"
   surf_spot4.url = "https://en.wikipedia.org/wiki/Beaver_Creek_Resort"
-  surf_spot4.surf_shop = "TBD"
-  surf_spot4.food = "TBD"
+  surf_spot4.shop = "Best Surf Shop"
+  surf_spot4.shop_url = "https://www.surfshop.com"
+  surf_spot4.food = "Eat Here!"
+  surf_spot4.food_url = "https://www.lunchbucket.com"
   
   surf_spot5 = self.new 
   surf_spot5.name = "Huntington Beach Pier, Huntington Beach"
   surf_spot5.url = "https://en.wikipedia.org/wiki/Breckenridge_Ski_Resort"
-  surf_spot5.surf_shop = "TBD"
-  surf_spot5.food = "TBD"
+  surf_spot5.shop = "Best Surf Shop"
+  surf_spot5.shop_url = "https://www.surfshop.com"
+  surf_spot5.food = "Eat Here!"
+  surf_spot5.food_url = "https://www.lunchbucket.com"
   
   surf_spot6 = self.new 
   surf_spot6.name = "Surfrider Beach, Malibu"
   surf_spot6.url = "https://en.wikipedia.org/wiki/Buttermilk_(ski_area)"
-  surf_spot6.surf_shop = "TBD"
-  surf_spot6.food = "TBD"
+  surf_spot6.shop = "Best Surf Shop"
+  surf_spot6.shop_url = "https://www.surfshop.com"
+  surf_spot6.food = "Eat Here!"
+  surf_spot6.food_url = "https://www.lunchbucket.com"
   
   surf_spot7 = self.new 
   surf_spot7.name = "Rincon Point, Carpinteria"
   surf_spot7.url = "https://en.wikipedia.org/wiki/Copper_Mountain_(Colorado)"
-  surf_spot7.surf_shop = "TBD"
-  surf_spot7.food = "TBD"
+  surf_spot7.shop = "Best Surf Shop"
+  surf_spot7.shop_url = "https://www.surfshop.com"
+  surf_spot7.food = "Eat Here!"
+  surf_spot7.food_url = "https://www.lunchbucket.com"
   
   surf_spot8 = self.new 
   surf_spot8.name = "Pismo Beach Pier, Pismo Beach"
   surf_spot8.url = "TBD"
-  surf_spot8.surf_shop = "TBD"
-  surf_spot8.food = "TBD"
+  surf_spot8.shop = "Best Surf Shop"
+  surf_spot8.shop_url = "https://www.surfshop.com"
+  surf_spot8.food = "Eat Here!"
+  surf_spot8.food_url = "https://www.lunchbucket.com"
   
   surf_spot9 = self.new 
   surf_spot9.name = "Asilomar State Beach, Pacific Grove"
   surf_spot9.url = "TBD"
-  surf_spot9.surf_shop = "TBD"
-  surf_spot9.food = "TBD"
+  surf_spot9.shop = "Best Surf Shop"
+  surf_spot9.shop_url = "https://www.surfshop.com"
+  surf_spot9.food = "Eat Here!"
+  surf_spot9.food_url = "https://www.lunchbucket.com"
   
   surf_spot10 = self.new 
   surf_spot10.name = "Mavericks, Half Moon Bay"
   surf_spot10.url = "TBD"
-  surf_spot10.surf_shop = "TBD"
-  surf_spot10.food = "TBD"
+  surf_spot10.shop = "Best Surf Shop"
+  surf_spot10.shop_url = "https://www.surfshop.com"
+  surf_spot10.food = "Eat Here!"
+  surf_spot10.food_url = "https://www.lunchbucket.com"
 
 
   [surf_spot1, surf_spot2, surf_spot3, surf_spot4, surf_spot5, surf_spot6, surf_spot7, surf_spot8, surf_spot9, surf_spot10]
