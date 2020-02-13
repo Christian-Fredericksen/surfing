4091b8828bcf64cd6f6b4265e8c0b5c352b80369
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-02-13 01:50:33 +0000

    stuff

--- lib/surfing/surf.rb
+++ lib/surfing/surf.rb
@@ -21,25 +21,33 @@ class Surf
   end 
     
     
-  def self.scrape_beach
-    shreck = self.new 
+  # def self.scrape_beach
+  #   shreck = self.new 
+  #   html = open('https://wanderwisdom.com/travel-destinations/A-Locals-Guide-to-Orange-County-Beaches')
+  #   doc = Nokogiri::HTML(html)
+  #   name = doc.css("h2.subtitle")
+  #   the_name = name.map{|title| title.text.strip}
+  #   # binding.pry 
+  # end
+   
+  # def self.scrape_info
+  #   donkey = self.new 
+  #   html = open('https://localemagazine.com/beginner-surf-spots/')
+  #   doc = Nokogiri::HTML(html)
+  #   info = doc.css("p span span")
+  #   the_info = info.map{|info| info.text.strip}
+  #   #binding.pry
+  # end 
+  
+  def self.scrapped_data
     html = open('https://wanderwisdom.com/travel-destinations/A-Locals-Guide-to-Orange-County-Beaches')
     doc = Nokogiri::HTML(html)
-    name = doc.css("h2.subtitle")
-    the_name = name.map{|title| title.text.strip}
-    # binding.pry 
-   end
-   
-   def self.scrape_info
-    donkey = self.new 
-    html = open('https://localemagazine.com/beginner-surf-spots/')
-    doc = Nokogiri::HTML(html)
-    info = doc.css("p span span")
-    the_info = info.map{|info| info.text.strip}
-    #binding.pry
-   end 
-  
-    
+    container = doc.css("div.full.module.moduleText")
+    container.each do |b|
+      title = b.css("h2.subtitle").text
+      info = b.css("p").text
+      Surf.new(name, stats)
+  end  
   
   
 end 
