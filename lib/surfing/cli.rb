5350f5caf1dd89c27f2b865e7201e823a033fcd8
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-02-13 19:52:22 +0000

    more stuff

--- lib/surfing/cli.rb
+++ lib/surfing/cli.rb
@@ -13,13 +13,12 @@ class Surfing::CLI
   
    def menu
      input = nil
-     puts "\n\nBruh! Wanna see a list of pier pounders? Just enter 'list' 
-or type 'later' to exit the program.\n\n"
+     puts "\n\nBruh! Wanna see a list of pier pounders?\nJust enter 'list'\nor type 'later' to exit the program.\n\n"
     while input != "later"
     input = gets.strip.downcase
-      if  input.to_i > 0 && input.to_i < Surf.scrapped_data.length+1
-      puts Surf.all[input.to_i-1]
-       elsif input == "list"
+      # if  input.to_i > 0 && input.to_i < Surf.scrapped_data.length+1
+      # puts Surf.all[input.to_i-1]
+       if input == "list"
          list_beaches
        elsif input == "later"
          shaka
@@ -32,7 +31,7 @@ or type 'later' to exit the program.\n\n"
   
   
   def list_beaches
-    
+    input = nil
     puts "\n\nCheck out this list!\nFrom catching 'the drop'\nto getting 'worked',\nthese are the sets to catch!"
      puts "(in no particular order)\n\n"
       @beaches = Surf.all
@@ -40,6 +39,16 @@ or type 'later' to exit the program.\n\n"
         puts "#{i}. #{beach.name}"
       end
       puts "\nWanna 'get wet'?\nEnter a digit, dude.\nOr 'later' to bail.\n\n"
+      while input != "later"
+    input = gets.strip.downcase
+      if  input.to_i > 0 && input.to_i < Surf.scrapped_data.length+1
+      puts "BEACH: #{Surf.name}\n#{Surf.info}"
+       elsif input == "later"
+         shaka
+       else 
+         puts "\nWHOA! Bad move, Barney! Try again or say 'later':\n\n"
+       end
+     end
   end
   
    
