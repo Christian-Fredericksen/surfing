f075ddcc6aca12b9b854568819923e5eeaf8c793
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-02-14 20:22:48 +0000

    closer

--- lib/surfing/cli.rb
+++ lib/surfing/cli.rb
@@ -1,13 +1,18 @@
 class Surfing::CLI 
+  
+  attr_accessor :name, :info
+  
   def call
     Surf.scraped_data
     greeting  
   end
+  
   def greeting
     puts "\n\nWelcome to the 10 MOST RADICAL and GNARLY surf spots EVER in California!"
     puts "(Hyperbole Rules!)"
     menu 
   end 
+  
    def menu
      puts "\n\nBruh! Wanna see a list of pier pounders?\nJust enter 'list'\nor type 'later' to exit the program.\n\n"
      input = gets.strip.downcase
@@ -18,40 +23,61 @@ class Surfing::CLI
        else 
          puts "\nWHOA! Bad move, Barney! Try again or say 'later':\n\n"
          menu
+        
        end
-   end 
+   end
+   
+   @@all = []
+   
   def list_beaches
     puts "\n\nCheck out this list!\nFrom catching 'the drop'\nto getting 'worked',\nthese are the sets to catch!"
     puts "(in no particular order)\n\n"
       @beaches = Surf.all
       @beaches.shift
       @beaches.pop
+      @@all << @beaches
       @beaches.each.with_index(1) do |beach, i|
         puts "#{i}. #{beach.name}"
       end
       puts "\nWanna 'get wet'?\nEnter a digit, dude.\nOr 'later' to bail.\n\n"
       surf_entry
-    end 
-     def surf_entry
+  end 
+  
+  def surf_entry
       input = gets.strip
-      if input.to_i.between?(0, Surf.all.count) && input != input.to_s
+        if input.to_i > 0 && input.to_i <= Surf.all.count
         show_info(input)
-      elsif input.downcase == "later"
+       
+        elsif input == "later"
         shaka
-      else
-        puts "\nWHOA! Bad move, Barney! Try again or say 'later'\n\n"
+        elsif input == "list"
+        re_list
+        
+        else 
+          puts "\nWHOA! Bad move, Barney! Try again or say 'later'\n\n"
         surf_entry
-      end
-    end 
+        end
+  end
+     
+     def re_list
+        @@all.each.with_index(1) do |beach, i|
+        puts "#{i}. #{beach.name}"
+     end
+     puts "\nWanna 'get wet'?\nEnter a digit, dude.\nOr 'later' to bail.\n\n"
+      surf_entry
+   end 
+     
   def show_info(input)
     d = Surf.find(input)
           puts "\nBEACH:\n#{d.name}\n\nINFO:\n#{d.info}\n\n"
-          puts "\nWanna see something else?\nEnter a digit, dude.\nOr 'later' to bail.\n\n"
+          puts "\nWanna see something else?\nEnter a digit, dude.\nOr 'list' to see them all again.\nOr 'later' to bail.\n\n"
           surf_entry
   end 
+  
    def shaka 
      puts "\nHave a great time in California!!\n\n"
    end
+   
 end 
 
 
